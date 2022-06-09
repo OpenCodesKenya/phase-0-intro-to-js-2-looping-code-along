c7de4151ee8d75ae887eed2e96eee10858d6ca37
Sarah Mukuti <cesarwrites24@gmail.com>
2022-06-09 08:27:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names){
+    let thankYou = [];
+    for (let i =0; i < names.length; i++){
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYou;
+}
+
+function countDown(num1){
+    while (num1 > 0){
+        console.log(num1);
+        num1 --;
+    }
+    console.log(num1);
+}
